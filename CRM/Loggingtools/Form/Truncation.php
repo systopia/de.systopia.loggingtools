fce0048fbf71574c58b8876a9d24cf1f6a4de5d4
B. Zschiedrich <zschiedrich@systopia.de>
2021-01-10 18:09:50 +0100

    Basic form for truncation

---
+++ CRM/Loggingtools/Form/Truncation.php
@@ -0,0 +1,104 @@
+<?php
+
+/*-------------------------------------------------------+
+| SYSTOPIA LOGGING TOOLS EXTENSION                       |
+| Copyright (C) 2020 SYSTOPIA                            |
+| Author: B. Zschiedrich (zschiedrich@systopia.de)       |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++--------------------------------------------------------*/
+
+use CRM_Loggingtools_ExtensionUtil as E;
+
+/**
+ * Form for truncating the logging tables.
+ *
+ * @see https://docs.civicrm.org/dev/en/latest/framework/quickform/
+ */
+class CRM_Loggingtools_Form_Truncation extends CRM_Core_Form
+{
+    public function buildQuickForm()
+    {
+        parent::buildQuickForm();
+
+        CRM_Utils_System::setTitle(E::ts("Logging Tools Truncation"));
+
+        $this->add(
+            'select',
+            'time_horizon',
+            E::ts('Choose the time horizon:'),
+            $this->getTimeHorizons(),
+            false,
+            ['class' => 'crm-select2 huge']
+        );
+
+        $this->add(
+            'datepicker',
+            'custom_time_horizon',
+            E::ts('Custom date:'),
+            $this->getTimeHorizons(),
+            false,
+            ['class' => 'huge disabled']
+        );
+
+        $this->addButtons(
+            [
+                [
+                    'type' => 'cancel',
+                    'name' => E::ts('Cancel'),
+                    'isDefault' => false,
+                ],
+                [
+                    'type' => 'submit',
+                    'name' => E::ts('Start'),
+                    'isDefault' => true,
+                ]
+            ]
+        );
+    }
+
+    public function postProcess()
+    {
+        parent::postProcess();
+
+        $values  = $this->exportValues();
+
+        // update logging tables
+        CRM_Loggingtools_LoggingTables::setExcludedTables($values['exclude_logging_tables']);
+
+        // Forward back to the previous page:
+        $targetUrl = html_entity_decode(CRM_Core_Session::singleton()->readUserContext());
+
+        CRM_Loggingtools_Queue_Runner_TruncatingLauncher::launchRunnerViaWeb(
+            $keepSinceDateTime,
+            $tableNames,
+            $cleanupDeletedEntities,
+            $targetUrl
+        );
+    }
+
+    /**
+     * Get a list of all possible time horizons for the truncation, including a custom one.
+     *
+     * @return array
+     *   map: number of months => label in human language
+     */
+    private function getTimeHorizons()
+    {
+        $result = [
+            6 => E::ts('Last %1 months', [1 => 6]),
+            12 => E::ts('Last %1 months', [1 => 6]),
+            24 => E::ts('Last %1 years', [1 => 2]),
+            60 => E::ts('Last %1 years', [1 => 5]),
+            'custom' => E::ts('Custom date'),
+        ];
+
+        return $result;
+    }
+}
---
+++ templates/CRM/Loggingtools/Form/Truncation.tpl
@@ -0,0 +1,46 @@
+{*-------------------------------------------------------+
+| SYSTOPIA LOGGING TOOLS EXTENSION                       |
+| Copyright (C) 2020 SYSTOPIA                            |
+| Author: B. Endres (endres@systopia.de)                 |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++-------------------------------------------------------*}
+
+{crmScope extensionKey='de.systopia.loggingtools'}
+
+<h3>{ts}Logging Tools Truncation{/ts}</h3>
+
+<p>
+    {ts}You can choose a time horizon for since when the logging table entries shall be kept. All entries older than the given time frame (or point in time) will be deleted.{/ts}
+</p>
+
+<br>
+
+<div class="crm-section">
+    <div class="label">{$form.time_horizon.label}</div>
+    <div class="content">{$form.time_horizon.html}</div>
+    <div class="clear"></div>
+</div>
+<div class="crm-section">
+    <div class="label">{$form.custom_time_horizon.label}</div>
+    <div class="content">{$form.custom_time_horizon.html}</div>
+    <div class="clear"></div>
+</div>
+
+<br>
+
+<p class="red warning">
+    {ts}WARNING: This process is irreversible and dangerous. It must not be haltet until full completion. Note that logging is disabled while truncating, meaning that every changes in the meantime will invalidate the logging tables.{/ts}
+</p>
+
+<div class="crm-submit-buttons">
+{include file="CRM/common/formButtons.tpl" location="bottom"}
+</div>
+
+{/crmScope}
